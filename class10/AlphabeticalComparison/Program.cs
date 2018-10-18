763ed45c8fd993f9000df7715b517fcea96cff47
paulolacdo-o <paulo.lacdo.o@gmail.com>
2018-10-17 20:09:06 -0500

    Added class10 exercises and homework.

---
+++ class10/AlphabeticalComparison/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace AlphabeticalComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, secondName;
+
+            Console.Write("Please enter the first name: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Please enter the second name: ");
+            secondName = Console.ReadLine();
+
+            switch (CompareNames(firstName, secondName))
+            {
+                case 1: Console.WriteLine($"The name {firstName} is first.");
+                    break;
+                case -1: Console.WriteLine($"The name {secondName} is first.");
+                    break;
+                case 0: Console.WriteLine($"The names are the same.");
+                    break;
+                default: Console.WriteLine("Armageddon!");
+                    break;
+            }
+
+
+            Console.Write("\nPress the [Enter] key to exit the program.");
+            Console.ReadLine();
+        }
+
+        static int CompareNames(string firstName, string secondName)
+        {
+            int result = 0;
+            int i = 0;
+
+            while (result == 0 && (i < firstName.Length && i < secondName.Length))
+            {
+                if((int)firstName[i] > (int)secondName[i])
+                {
+                    result = -1;
+                }
+                else if((int)firstName[i] < (int)secondName[i])
+                {
+                    result = 1;
+                }
+                i++;
+            }
+
+            if(result == 0)
+            {
+                if (firstName.Length < secondName.Length)
+                    result = 1;
+                else if (firstName.Length > secondName.Length)
+                    result = -1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ class10/AverageGrades/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AverageGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] studentGrades = new int[5, 3];
+            float classTotal = 0;
+            float[] studentAverage = new float[5];
+            float classAverage;
+
+            for(int i = 0; i < studentGrades.GetLength(0); i++)
+            {
+                int studentTotal = 0;
+                for(int j = 0; j < studentGrades.GetLength(1); j++)
+                {
+                    Console.Write($"Enter the grade of Student #{i + 1} for Quiz #{j + 1}: ");
+                    studentTotal += studentGrades[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                classTotal += studentAverage[i] = (float)studentTotal / studentGrades.GetLength(1);
+                Console.WriteLine($"The average of Student #{i+1} is {studentAverage[i]}.\n");
+            }
+
+            classAverage = classTotal / studentGrades.GetLength(0);
+            Console.WriteLine($"The average of the class is {classAverage}.\n");
+
+            Console.WriteLine("Press [Enter] key to exit program.");
+            Console.ReadLine();
+        }
+    }
+}
