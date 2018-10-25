8ea280b28d85540931fcee19042e0a3c2482dee8
paulolacdo-o <paulo.lacdo.o@gmail.com>
2018-10-25 18:23:30 -0500

    modified VoterValidity

---
+++ class12/VoterValidity/Person.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoterValidity
+{
+    class Person
+    {
+        private string Name;
+        private int Age, Id, Weight, Height;
+
+        public Person()
+        {
+            Name = "";
+            Age = Weight = Height = 0;
+            GenerateID();
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            GenerateID();
+        }
+
+        public Person(string name, int age, int weight, int height)
+        {
+            Name = name;
+            Age = age;
+            Weight = weight;
+            Height = height;
+            GenerateID();
+        }
+
+        public bool IsAllowedToVote()
+        {
+            if (Age < 18)
+                return false;
+
+            return true;
+        }
+
+        public bool ValidateName()
+        {
+            for(int i = 0; i < Name.Length; i++)
+            {
+                if (!Char.IsLetter(Name[i]) && Name[i] != ' ')
+                    return false;
+            }
+            return true;
+        }
+
+        override public string ToString()
+        {
+            return $"{Name}, ID {Id}, age is {Age}, weight is {Weight}, height is {Height} cm.";
+        }
+
+        public void GenerateID()
+        {
+            Id = new Random().Next(10000000, 99999999);
+        }
+
+        //mutators
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+        public void SetAge(int age)
+        {
+            Age = age;
+        }
+        public void SetWeight(int weight)
+        {
+            Weight = weight;
+        }
+        public void SetHeight(int height)
+        {
+            Height = height;
+        }
+
+        //accessors
+        public string GetName()
+        {
+            return Name;
+        }
+        public int GetAge()
+        {
+            return Age;
+        }
+        public int GetId()
+        {
+            return Id;
+        }
+        public int GetWeight()
+        {
+            return Weight;
+        }
+        public int GetHeight()
+        {
+            return Height;
+        }
+    }
+}
--- class12/VoterValidity/Program.cs
+++ class12/VoterValidity/Program.cs
@@ -6,7 +6,82 @@ namespace VoterValidity
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person[] voter = new Person[3];
+            
+            for (int i = 0; i < voter.Length; i++)
+            {
+                voter[i] = ReadVoter(i);
+            }
+
+            for (int i = 0; i < voter.Length; i++)
+            {
+
+                Console.WriteLine($"Voter #{i + 1} name is " + (voter[i].ValidateName() ? "valid" : "invalid") +
+                    " and is " + (voter[i].IsAllowedToVote() ? "of legal age." : "not of legal age."));
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < voter.Length; i++)
+            {
+                Console.WriteLine($"Voter #{i + 1} is " + voter[i].ToString());
+            }
+
+            Console.ReadKey();
+        }
+
+        static Person ReadVoter(int num)
+        {
+            Person person;
+            string name;
+            int age, height, weight;
+            switch (num)
+            {
+                case 0:
+                    Console.WriteLine("Please register the first voter: ");
+                    person = new Person();
+                    Console.Write("Please enter name: ");
+                    person.SetName(Console.ReadLine());
+                    Console.Write("Please enter age: ");
+                    person.SetAge(Convert.ToInt32(Console.ReadLine()));
+                    Console.Write("Please enter weight in kgs: ");
+                    person.SetWeight(Convert.ToInt32(Console.ReadLine()));
+                    Console.Write("Please enter height in cms: ");
+                    person.SetHeight(Convert.ToInt32(Console.ReadLine()));
+                    break;
+
+                case 1:
+                    Console.WriteLine("\nPlease register the second voter: ");
+                    Console.Write("Please enter name: ");
+                    name = Console.ReadLine();
+                    Console.Write("Please enter age: ");
+                    age = Convert.ToInt32(Console.ReadLine());
+                    person = new Person(name, age);
+                    Console.Write("Please enter weight in kgs: ");
+                    person.SetWeight(Convert.ToInt32(Console.ReadLine()));
+                    Console.Write("Please enter height in cms: ");
+                    person.SetHeight(Convert.ToInt32(Console.ReadLine()));
+                    break;
+
+                case 2:
+                    Console.WriteLine("\nPlease register the third voter: ");
+                    Console.Write("Please enter name: ");
+                    name = Console.ReadLine();
+                    Console.Write("Please enter age: ");
+                    age = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Please enter weight: ");
+                    weight = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Please enter height: ");
+                    height = Convert.ToInt32(Console.ReadLine());
+                    person = new Person(name, age, weight, height);
+                    break;
+
+                default:
+                    person = new Person();
+                    break;
+            }
+
+            return person;
         }
     }
 }
