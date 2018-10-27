3a684685e18a0b648f84727afdcd3ba86cb2ed84
paulolacdo-o <paulo.lacdo.o@gmail.com>
2018-10-27 07:54:28 -0500

    Added PasswordChecker in class12

---
+++ class12/PasswordChecker/Password.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordChecker
+{
+    class Password
+    {
+        private int _length;
+        private string _value;
+
+        public Password()
+        {
+            _length = 0;
+            _value = null;
+        }
+
+        public Password(int length)
+        {
+            _length = length;
+            GeneratePassword();
+        }
+
+        public void GeneratePassword()
+        {
+            Random rng = new Random();
+            string value = "";
+            
+            for(int i = 0; i < _length; i++)
+            {
+                value += (char)rng.Next(33, 126);
+            }
+
+            _value = value.ToString();
+        }
+
+        public bool IsStrong()
+        {
+            bool isStrong = false,
+                isLower = false,
+                isUpper = false,
+                isDigit = false,
+                isSymbol = false;
+
+            for(int i = 0; i < _length; i++)
+            {
+                if (char.IsSymbol(_value[i]))
+                    isSymbol = true;
+                else if (char.IsDigit(_value[i]))
+                    isDigit = true;
+                else if (char.IsLower(_value[i]))
+                    isLower = true;
+                else if (char.IsUpper(_value[i]))
+                    isUpper = true;
+            }
+            if (isUpper && isLower && isDigit && isSymbol)
+                isStrong = true;
+
+            return isStrong;
+        }
+
+        //accessors
+        public int GetLength()
+        {
+            return _length;
+        }
+        public string GetValue()
+        {
+            return _value;
+        }
+
+        //mutators
+        public void SetLength(int length)
+        {
+            _length = length;
+        }
+    }
+}
--- class12/PasswordChecker/Program.cs
+++ class12/PasswordChecker/Program.cs
@@ -6,7 +6,43 @@ namespace PasswordChecker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int length, numOfPasswords;
+
+            Console.Write("Please enter the number of characters to use for each password: ");
+            length = GetNumber();
+            Console.Write("Please enter the number of passwords to create: ");
+            numOfPasswords = GetNumber();
+            Console.WriteLine();
+
+            Password[] passwords = new Password[numOfPasswords];
+
+            Console.WriteLine("The passwords that were created are:\n");
+            for(int i = 0; i < numOfPasswords; i++)
+            {
+                passwords[i] = new Password(length);
+                Console.Write($"Password {i + 1}: ");
+                PrintPasswordInformation(passwords[i]);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void PrintPasswordInformation(Password password)
+        {
+            Console.Write($"\"{password.GetValue()}\" - " + (password.IsStrong()?"Strong":"Weak"));
+            Console.WriteLine();
+        }
+
+        static int GetNumber()
+        {
+            int number;
+
+            while(!Int32.TryParse(Console.ReadLine(),out number) || number < 1)
+            {
+                Console.Write("ERROR: Invalid value. Please try again: ");
+            }
+
+            return number;
         }
     }
 }
