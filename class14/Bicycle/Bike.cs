12de0bc36e6a0bd9e10253ab3491ebf0f3a980b7
paulolacdo-o <paulo.lacdo.o@gmail.com>
2018-10-31 18:30:00 -0500

    add class14

---
+++ class14/Bicycle/Bike.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bicycle
+{
+    class Bike
+    {
+        private int _speed;
+
+        public Bike(int speed)
+        {
+            _speed = speed;
+        }
+
+        public void Acccelerate(int additionalSpeed)
+        {
+            _speed += additionalSpeed;
+        }
+
+        public int GetSpeed()
+        { return _speed; }
+    }
+}
---
+++ class14/Bicycle/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Bicycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bike[] bikes = new Bike[10];
+            InitializeBikeArray(bikes);
+            Console.WriteLine("Bike information before acceleration:");
+            PrintBikeInformation(bikes);
+            Console.WriteLine();
+            AccelerateOddBikes(bikes);
+            Console.WriteLine("Bike information after acceleration:");
+            PrintBikeInformation(bikes);
+
+            Console.ReadKey();
+        }
+
+        static void InitializeBikeArray(Bike[] bikes)
+        {
+            Random rng = new Random();
+            for(int i = 0; i < bikes.Length; i++)
+            {
+                bikes[i] = new Bike(rng.Next(1, 10));
+            }
+        }
+
+        static void AccelerateOddBikes(Bike[] bikes)
+        {
+            for (int i = 0; i < bikes.Length; i++)
+            {
+                if(i%2 == 1)
+                {
+                    bikes[i].Acccelerate(10);
+                }
+            }
+        }
+
+        static void PrintBikeInformation(Bike[] bikes)
+        {
+            for (int i = 0; i < bikes.Length; i++)
+            {
+                Console.WriteLine($"Bike {i} has speed {bikes[i].GetSpeed()}");
+            }
+        }
+    }
+}
