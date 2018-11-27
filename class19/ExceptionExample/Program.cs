bf7dfed19d941e526f93e9732c30b4bfafdae25c
paulolacdo-o <paulo.lacdo.o@gmail.com>
2018-11-26 20:26:22 -0600

    Adding class19

---
+++ class19/ExceptionExample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ExceptionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = { 90, 95, 97 };
+            //int[] grades = new int[0];
+            //int[] grades = null;
+
+            float average;
+            string output;
+
+            try
+            {
+                average = GetGradesAverage(grades);
+                output = "The average is " + average;
+            }
+            catch (NullReferenceException ex)
+            {
+                output = "An exception has occurred: The object that has been passed is null";
+            }
+            catch (Exception ex)
+            {
+                output = "An exception has occurred: "+ ex.Message;
+            }
+
+
+
+            Console.WriteLine(output);
+
+            Console.ReadKey();
+        }
+
+        static float GetGradesAverage(int[] grades)
+        {
+            int total = 0;
+            if (grades.Length > 0)
+            {
+                for (int i = 0; i < grades.Length; i++)
+                {
+                    total += grades[i];
+                }
+            }
+            else
+            {
+                throw new Exception("The array that has been passed is empty.");
+            }
+
+            float average = (float)total / grades.Length;
+            return average;
+        }
+    }
+}
